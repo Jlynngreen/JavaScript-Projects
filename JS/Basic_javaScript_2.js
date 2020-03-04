1203eddda9e0ba93b88eef904788808b4087740a
Jlynngreen <jessgreen2012@gmail.com>
2020-03-04 14:14:58 -0700

    heeres go!

---
+++ JS/Basic_javaScript_2.js
@@ -0,0 +1,24 @@
+alert("Hello Peeps");
+
+var A = "Welcome Salt lake";
+document.write(A);
+
+var x= "The ones who love us never really leaves us";
+document.write(x)
+
+document.write("But even a bad cup of coffee is better than no coffee at all."); 
+
+var Countries= "The Move", Venezuela = "State", Texas = "Utah";
+    document.write(Venezuela);
+
+var yellow = "I love yellow";
+var yellow = yellow.fontcolor("yellow");
+document.write(yellow);
+
+Document.write(25 + 25);
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
