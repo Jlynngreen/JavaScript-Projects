fb99fef12c7c26b338006e483dcaf91592c9ca02
Jlynngreen <jessgreen2012@gmail.com>
2020-03-04 12:34:31 -0700

    Java

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,22 @@
+alert("Hello, World!");
+
+var A = "This is a string";
+document.write(A);
+
+var x= "we are the so-called \"vikings\" from the north.";
+document.write(x)
+
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\t matter and those who matter don\t mind.\""
++"-Dr. Seuss"); 
+
+var Family= "The Greens", Dad = "Gary", Mom = "Frances",
+    Daughter = "Jess", Son = "Travis", Son = "Jacob", Son = "Lucas";
+    document.write(Dad);
+
+var teal = "I love teal.";
+var teal = teal.fontcolor("teal");
+document.write(teal);
+
+Document.write(5 +5);
+
