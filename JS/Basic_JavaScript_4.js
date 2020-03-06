daa0f6584981a3487d27a6fcc98d4479ee26dcb6
Jlynngreen <jessgreen2012@gmail.com>
2020-03-06 14:12:07 -0700

    project 4

---
+++ JS/Basic_JavaScript_4.js
@@ -0,0 +1,34 @@
+function my_Dictionary() {
+    var Animal = {
+        Species: "Horse",
+        Color: "Brown",
+        Breed: "Buckskin",
+        Age: "2",
+        Sound: "Neigh",
+    }
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
+
+function my_Dictionary() {
+    var Automobile = {
+        Model: "Camry",
+        Make: "Toyota",
+        Color: "Red",
+        Year: "2020",
+        Sound: "roar",
+    }
+    document.getElementById("Dictionary").innerHTML = Automobile.Sound;
+}
+
+
+function my_Dictionary() {
+    var Animal = {
+        Species: "Dog",
+        Color: "Black",
+        Breed: "Labrador",
+        Age: "5",
+        Sound: "Bark",
+ };
+ delete Animal.Sound;
+ document.getElementById("Dictionary").innerHTML = Animal.sound;
+}
