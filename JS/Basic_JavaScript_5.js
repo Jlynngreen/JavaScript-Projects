96feb868c5f3d8f966b0f4cbc2ef68bea6294600
Jlynngreen <jessgreen2012@gmail.com>
2020-03-09 12:36:12 -0600

    java5

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,42 @@
+function my_Function () {
+    document.getElementById("Test").innerHTML = 0/0;   
+}
+document.write(-3E310);
+
+document.write(15 < 25);
+
+console.log(2 +2);
+
+document.write("10" + 5);
+
+document.write("15"+ 25); console.log(4 +4); document.write(10 < 2);
+
+document.write (10 == 10);
+
+document.write (3 == 11);
+
+X = 10;
+Y = 10;
+document.write (X === Y);
+
+
+X = 83;
+Y = "83";
+
+document.write(X === Y);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function () {
+    document.getElementById("Not").innerHTML = !(5 > 10);
+}
+
+function not_Function () {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
