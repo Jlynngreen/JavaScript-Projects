4b6a2c81f82e1eaea26627084d7dc3186f3243cf
Jlynngreen <jessgreen2012@gmail.com>
2020-03-19 17:02:12 -0600

    TicTacToe Project

---
+++ JS/Basic_JavaScript_11.js
@@ -0,0 +1,4 @@
+function displayType(character) {
+    var characterType = character.getAttribute("data-character-typw");
+    alert(characterType + " is in the " + character.innerHTML + " universe!"):
+}
